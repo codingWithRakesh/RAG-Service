11e38f24d1e83fd932c1f251fb11b49fd7e0f967
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2026-05-14 00:10:39 +0530

    feat: Implement user management and authentication features
    - Added User entity with fields for email, name, password, and authentication provider.
    - Created AuthProviderType and Payment enums for user authentication and payment plans.
    - Developed ApiError class for standardized error responses.
    - Implemented GlobalExceptionHandler for handling exceptions globally.
    - Created repositories for User, DocumentData, and Message entities.
    - Developed AuthUtil for JWT token generation and validation.
    - Implemented CustomUserDetailsService for loading user details.
    - Created JwtAuthFilter for JWT authentication in requests.
    - Developed OAuth2SuccessHandler for handling successful OAuth2 logins.
    - Configured WebSecurity for securing endpoints and handling CORS.
    - Implemented DocumentDataService and FileUploadService for document handling.
    - Developed MessageService for managing user messages.
    - Created UserService for user-related operations including OAuth2 login handling.
    - Implemented utility classes for chunking and querying in RAG.
    - Added application properties and YAML configuration for environment variables.
    - Created initial test class for application context loading.

---
+++ main_server/src/main/java/com/mainServer/main_server/controller/DocumentDataController.java
@@ -0,0 +1,36 @@
+package com.mainServer.main_server.controller;
+
+import com.mainServer.main_server.dto.request.DocumentDataRequestDto;
+import com.mainServer.main_server.dto.request.URLRequestDto;
+import com.mainServer.main_server.service.DocumentDataService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/uploadData")
+@AllArgsConstructor
+public class DocumentDataController {
+
+    private final DocumentDataService documentDataService;
+
+    @PostMapping(
+            value = "/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<Map<String, Object>> uploadDocumentData(@Valid @ModelAttribute DocumentDataRequestDto dataRequestDto) throws IOException {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(documentDataService.uploadDocumentData(dataRequestDto));
+    }
+
+    @PostMapping("/upload-web-url")
+    public ResponseEntity<Map<String, Object>> uploadWebURL(@RequestBody URLRequestDto webURLRequest) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(documentDataService.uploadWebURL(webURLRequest));
+
+    }
+}
---
+++ main_server/src/main/java/com/mainServer/main_server/dto/request/URLRequestDto.java
@@ -0,0 +1,12 @@
+package com.mainServer.main_server.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class URLRequestDto {
+    private String url;
+}
